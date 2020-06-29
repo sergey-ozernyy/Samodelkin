700012a4f61bb581c04ea3cb18f1b575168653dc
sergey <sergey.ozernyy@mail.ru>
2020-06-29 22:47:51 +0300

    Книга пройдена...

--- app/src/main/java/android/bignerdranch/com/CharacterGenerator.kt
+++ app/src/main/java/android/bignerdranch/com/CharacterGenerator.kt
@@ -1,5 +1,13 @@
 package android.bignerdranch.com
 
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
+import kotlinx.serialization.Serializable
+import java.net.URL
+
+private const val CHARACTER_DATA_API = "https://chargen-api.herokuapp.com/"
+
+
 private fun <T> List<T>.rand() = shuffled().first()
 private fun Int.roll() = (0 until this)
     .map{(1..6).toList().rand()}
@@ -7,19 +15,37 @@ private fun Int.roll() = (0 until this)
     .toString()
 private val firstName = listOf("Eli", "Alex", "Sophie")
 private val lastName = listOf("Lightweaver", "Greatfoot", "Oakenfeld")
-object CharacterGenerator {    data class CharacterData(val name: String,
-                                                        val race: String,
-                                                        val dex: String,
-                                                        val wis: String,
-                                                        val str: String)
+
+
+object CharacterGenerator {
+    data class CharacterData (val name: String,
+                             val race: String,
+                             val dex: String,
+                             val wis: String,
+                             val str: String):java.io.Serializable
+
     private fun name() = "${firstName.rand()} ${lastName.rand()}"
     private fun race() = listOf("dwarf", "elf", "human", "halfling").rand()
     private fun dex() = 4.roll()
     private fun wis() = 3.roll()
     private fun str() = 5.roll()
+
     fun generate() = CharacterData(name = name(),
                                                 race = race(),
                                                 dex = dex(),
                                                 wis = wis(),
                                                 str = str())
+
+    fun fromApiData(apiData: String): CharacterData {
+        val (race, name, dex, wis, str) = apiData.split(",")
+        return CharacterData(name, race, dex, wis, str)
+    }
+
+
+}
+
+fun fetchCharacterData(): Deferred<CharacterGenerator.CharacterData>{
+    return async {
+        val apiData = URL(CHARACTER_DATA_API).readText()
+        CharacterGenerator.fromApiData(apiData)}
 }
--- app/src/main/java/android/bignerdranch/com/NewCharacterActivity.kt
+++ app/src/main/java/android/bignerdranch/com/NewCharacterActivity.kt
@@ -2,11 +2,55 @@ package android.bignerdranch.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+
+
+private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
+
+private var Bundle.characterData
+        get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+        set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
 
 class NewCharacterActivity : AppCompatActivity() {
+    private var characterData = CharacterGenerator.generate()
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.characterData = characterData
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        characterData = savedInstanceState?.characterData ?:
+            CharacterGenerator.generate()
+        generateButton.setOnClickListener{
+            launch(UI) {
+                characterData = fetchCharacterData().await()
+                displayChracterData()
+            }
+
+        }
+
+        displayChracterData()
+
+    }
+
+    private fun displayChracterData(){
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
     }
 }
